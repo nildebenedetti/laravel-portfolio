e7987ff3fa8ca378eecf14bd6be0bb8472e49258
“Nilde <nilde.benedetti@gmail.com>
2026-08-27 16:25:58 +0200

    add Project model, controller, migration, and seeder for projects

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i <10; $i++) {
+            
+        $newProject = new Project();
+
+        $newProject->title = $faker->words(3, true);
+        $newProject->{'tech stack'} = $faker->sentence();
+        $newProject->{'github link'} = $faker->url();
+        $newProject->client = $faker->company();
+        $newProject->description = $faker->paragraphs(4, true);
+
+        $newProject->save();
+        }
+
+    }
+}
