5e49e4fd1fe5decea1eb7a592b8ca226dfc6c81b
“Nilde <nilde.benedetti@gmail.com>
2026-08-27 12:36:26 +0200

    add resource routing for posts to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,5 +31,8 @@ Route::middleware([ 'auth', 'verified' ])
 
 
     });
+
+
+Route::resource("posts", PostController::class);
 
 require __DIR__.'/auth.php';
