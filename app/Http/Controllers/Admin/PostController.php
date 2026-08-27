a73e428391b9dc81f16644ad68fb40fc7c00fd17
“Nilde <nilde.benedetti@gmail.com>
2026-08-27 15:53:27 +0200

    add PostController and resource routing for posts with authentication middleware

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +33,7 @@ Route::middleware([ 'auth', 'verified' ])
     });
 
 
-Route::resource("posts", PostController::class);
+Route::resource("posts", PostController::class)
+    ->middleware("auth", "verified");
 
 require __DIR__.'/auth.php';
