6ca77105752c5e1ee1b6d790a6225a3c3cc717cd
“Nilde <nilde.benedetti@gmail.com>
2026-08-27 11:05:37 +0200

    add Model for Posts resource, migration for populating posts table and PostsTableSeeder

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+
+        $newPost = new Post();
+
+        $newPost->title = $faker->sentence();
+        $newPost->author = $faker->name();
+        $newPost->category = $faker->word();
+        $newPost->content = $faker->paragraph();
+
+        $newPost->save();
+
+        }
+    }
+}
