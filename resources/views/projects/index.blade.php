b6d24a7c4791ec09be700a0728e491c2f9f88905
“Nilde <nilde.benedetti@gmail.com>
2026-08-31 17:28:08 +0200

    add button to navigate to project creation page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,7 +5,11 @@
 @section("content")
 
     <div class="container py-5">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        <!-- New Project BTN -->
+        <div class="d-flex justify-content-end py-4 gap-2">
+            <a class="btn btn-outline-primary" href="{{ route('projects.create') }}">Add New + </a>
+        </div>
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-4">
         @foreach($projects as $project)
             <div class="col">
                 <x-project-card :project="$project" />
