7f4324afdd43688d7e6d399d7b135eaffcb3251b
“Nilde <nilde.benedetti@gmail.com>
2026-08-31 16:31:43 +0200

    added protected variable with fillables for expliciting the fields present in db

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'github_link',
+        'tech_stack',
+        'client',
+        'description'
+    ]
 }
