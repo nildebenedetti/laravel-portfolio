fe24d320033023600ffec7bea99edbbde8e00854
“Nilde <nilde.benedetti@gmail.com>
2026-08-31 15:51:34 +0200

    add return of edit view and single project id in edit action of projectController

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -57,9 +57,9 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project) 
     {
-        //
+        return view("projects.edit", compact('project'));
     }
 
     /**
