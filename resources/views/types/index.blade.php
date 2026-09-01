9584272ed88de9ec637e56d342b59eb60ad098f4
“Nilde <nilde.benedetti@gmail.com>
2026-09-01 15:50:36 +0200

    add table for index route of types

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -3,8 +3,36 @@
 @section("title", "All Categories")
 
 @section("content")
-Ciao sono la iundex di types
-@dd($types)
+<div class="container py-5">
+    <!-- New Project BTN -->
+    <div class="d-flex justify-content-end py-4 gap-2">
+        <a class="btn btn-outline-primary" href="{{ route('types.create') }}">Add New + </a>
+    </div>
+    <!--  Types Table -->
+    <table class="table">
+        <thead>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Actions</th>    
+        </thead>
+        <tbody>
+            @foreach ($types as $type) 
+                <tr>
+                    <td>{{ $loop->iteration }} </td>
+                    <td>{{ $type->name }}</td>
+                    <td>{{ $type->description }}</td>
+                    <td> 
+                        <a href="#" class="btn btn-outline-warning"><i class="bi bi-pencil-fill"></i></a>
+                        <a href="#" class="btn btn-outline-danger"><i class="bi bi-trash3-fill"></i></a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
+</div>
 @endsection
 
 
