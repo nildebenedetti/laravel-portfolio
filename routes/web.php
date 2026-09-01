6b51f111e9852a6b88414493988da80bcc80eccb
“Nilde <nilde.benedetti@gmail.com>
2026-09-01 15:18:06 +0200

    add resource route fotr types

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\TypeController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,9 @@ Route::middleware([ 'auth', 'verified' ])
 
 
 Route::resource("projects", ProjectController::class)
+    ->middleware("auth", "verified");
+
+Route::resource("types", TypeController::class)
     ->middleware("auth", "verified");
 
 require __DIR__.'/auth.php';
