c49e62867edb6a720499956cc019b096f93f39da
“Nilde <nilde.benedetti@gmail.com>
2026-09-01 15:50:15 +0200

    add + isntall bootstrap icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 // import './bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
 
 import * as bootstrap from 'bootstrap';
